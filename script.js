ce6e6f8385626e6d0602073a15b4602ac6817b41
Fran Devops <francoapagnotta@gmail.com>
2021-04-28 00:27:57 -0300

    px to em changes

---
+++ script.js
@@ -0,0 +1,240 @@
+// * {
+//     box-sizing: border-box;
+//     margin: 0;
+//     padding: 0;
+// }
+
+
+// body {
+//     font-family: 'Ultra', serif;
+//     font-family: 'Vidaloka', serif;
+//     background-image: url("https://francopagnotta.github.io/Moonbucks/img/fondo2.jpg");
+//     background-position:center;
+//     background-attachment: fixed;
+//     background-size: cover;
+// }
+
+// .container {
+//   display: flex;
+//   justify-content: center;
+//   background-size: cover;
+//   position: relative;
+// }
+
+// .div-img {
+//     display: flex;
+//     width: 100%;
+//     height: 100vh;
+//     object-fit: cover;
+// }
+
+// .img {
+//     object-fit:cover;
+// }
+
+// .form {
+//     margin: 50px auto;
+//     width:600px; 
+//     height: 920px;
+//     background-color: rgba(255,255,255,.3);
+//     border: 1px solid rgba(255,255,255,1);
+//     border-radius: 10px;
+//     /* background: red; */
+//     text-align: center;
+//     position: absolute;
+// }
+
+// .form h1 {
+//     margin-top: 50px;
+//     display: block;
+//     font-size:70px;
+//     text-align: center;
+//     font-weight: lighter;
+//     letter-spacing: 5px;
+//     color: rgb(0, 0, 0);
+// }
+
+
+// .form span {
+//     margin: 50px;
+//     display: block;
+//     font-size:35px;
+//     text-align: center;
+//     letter-spacing: 3px;
+//     color: rgba(0,0,0,.9);
+// }
+
+// .form small {
+//     font-size: 20px;
+    
+// }
+
+// .form {
+//     margin-top: 70px;
+// }
+
+// .inputs-form {
+//     text-align: center;
+//     font-size: 25px;
+// }
+
+// .form input {
+//     display: block;
+//     margin: 20px auto;
+//     padding: 20px;
+//     width: 400px;
+//     border-radius: 10px;
+//     background-color: rgba(180,180,180,.7);
+//     border: 1px solid rgba(255,255,255,.8);
+//     font-size:25px;
+//     outline: none;
+// }
+
+// .form input:hover {
+//     background-color: rgba(255,2555,255,.7);
+//     transition: .4s ease-in-out;
+//     outline: none;
+// }
+
+// .form button {
+//     display: block;
+//     margin: 20px auto;
+//     padding: 20px;
+//     width: 200px;
+//     border-radius: 10px;
+//     background-color: rgba(180,180,180,.7);
+//     border: 1px solid rgba(255,255,255,1);
+//     font-family: 'Ultra', serif;
+//     font-family: 'Vidaloka', serif;
+//     font-size:25px;
+//     color: rgb(0, 0, 0);
+// }
+
+// .form button:hover {
+//     background-color: rgba(180,180,180,.1);
+//     font-size: 25px;
+//     letter-spacing: 2px;
+//     transition: .4s ease-in-out;
+//     outline: none;
+//     width: 400px;
+//     background-color: rgba(102,205,170, .5);
+// }
+
+// /* .form button:active {
+//     background-color: rgba(180,180,180,.1);
+//     font-size: 25px;
+//     letter-spacing: 2px;
+//     transition: .4s ease-in-out;
+//     outline: none;
+//     width: 400px;
+//     background-color: rgba(102,205,170, .5);
+// } */
+
+// .div-links {
+//     text-align: center;
+// }
+
+// .div-links > a {
+//     display: inline-block;
+//     margin: 50px;
+//     font-size: 50px;
+//     color: rgba(0,0,0,.9);
+//     list-style: none;
+// }
+
+// .fa-github:hover {
+//     color: rgba(255, 68, 0,.8);
+//     transition: .3s ease;
+// }
+
+// .fa-linkedin:hover {
+//     color: rgba(30, 150, 255,.8);
+//     transition: .3s ease;
+// }
+
+// .fa-google:hover {
+//     color: rgba(200, 34, 34,.8);
+//     transition: .3s ease;
+// }
+
+
+
+
+// /* RESPONSIVE DESIGN */
+
+// @media (max-width:800px) {
+//     .container {
+//         margin-top: -50px;
+//     }
+    
+//    body {
+//        background-position:inherit;
+//    }
+   
+//     .form {
+//         width: 250px;
+//         height: 450px;
+//     }
+
+//     .inputs-form {
+//         margin-top: 25px;
+//     }
+
+//     .form h1 {
+//         margin-top: 15px;
+//         display: block;
+//         font-size:35px;
+//         text-align: center;
+//         font-weight: lighter;
+//         letter-spacing: 5px;
+//         color: rgb(0, 0, 0);
+//     }
+    
+    
+//     .form span {
+//         margin: 5px;
+//         font-size:15px;
+//     }
+    
+//     .form input {
+//         display: block;
+//         margin: 10px auto;
+//         padding: 5px;
+//         width: 200px;
+//         font-size:15px;
+//     }
+
+//     .inputs-form label {
+//         font-size: 20px;
+//     }
+
+//     .form small {
+//         font-size: 15px;
+//     }
+    
+//     .form button {
+//         margin: 20px auto;
+//         padding: 5px;
+//         width: 100px;
+//         font-size:15px;
+//     }
+    
+//     .div-button button:active {
+//         width: 200px;
+//         padding: 5px;
+//         font-size: 15px;
+        
+//     }
+//     .div-links {
+//         display: flex;
+//         justify-content: center;
+//         margin-top: -50px;
+//     }
+
+//     .div-links > a {
+//         display: inline-block;
+//         font-size: 30px;
+//         margin-left: auto;
+//         margin-right: auto;
+//     }
+// }
